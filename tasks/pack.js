4a92fa769b6138cebc8929cec7ecc7cd8f2fa6c0
Ajay Sreedhar <sree.ajay468@outlook.com>
2019-09-02 23:28:32 +0530

    change: use require in global scope

--- tasks/pack.js
+++ tasks/pack.js
@@ -1,8 +1,16 @@
 'use strict';
 
-module.exports = function (gulp, packageJson) { 'use strict';
-    const _ = require('underscore');
-    const electronPackager = require('electron-packager');
+const _ = require('underscore');
+const ePackager = require('electron-packager');
+
+/**
+ * Prepares options and initiates build depending on architecture.
+ *
+ * @param {Gulp} gulp the gulp import
+ * @param {{author: string, name: string,
+ * version: string, main: string}} packageJson package.json file contents
+ */
+module.exports = function (gulp, packageJson) {
 
     const RELEASE_SETTINGS = {
         dir: '.',
@@ -38,14 +46,14 @@ module.exports = function (gulp, packageJson) { 'use strict';
     };
 
     gulp.task('pack-linux', (next) => {
-        electronPackager(_.extend(RELEASE_SETTINGS, {
+        ePackager(_.extend(RELEASE_SETTINGS, {
             platform: 'linux',
             arch: 'x64'
         }), next);
     });
 
     gulp.task('pack-windows', (next) => {
-        electronPackager(_.extend(RELEASE_SETTINGS, {
+        ePackager(_.extend(RELEASE_SETTINGS, {
             platform: 'win32',
             arch: 'x64',
             icon: 'resources/icons/kongdash-256x256.ico'
@@ -53,7 +61,7 @@ module.exports = function (gulp, packageJson) { 'use strict';
     });
 
     gulp.task('pack-osx', (next) => {
-        electronPackager(_.extend(RELEASE_SETTINGS, {
+        ePackager(_.extend(RELEASE_SETTINGS, {
             appBundleId: 'io.kongdash',
             platform: 'darwin',
             arch: 'all',
