963c759044d3659e7c0063c9b855b3154e34af71
Ajay Sreedhar <sree.ajay468@outlook.com>
2019-11-30 00:15:53 +0530

    TODO: fix gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,5 @@
 const gulp = require('gulp');
 const packageJson = require('./package.json');
 
-require('gulp-task-list')(gulp);
-
 require('./tasks/start')(gulp);
 require('./tasks/pack')(gulp, packageJson);
